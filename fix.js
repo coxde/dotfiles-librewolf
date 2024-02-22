deddf63b950464ef511da3ae062186ef0c4c9618
COxDE <63153334+coxde@users.noreply.github.com>
2024-02-22 21:08:21 +0000

    fix: fix running out of disk space (hopefully)

--- fix.js
+++ fix.js
@@ -1,1 +1,2 @@
 user_pref("media.webrtc.hw.h264.enabled",                               true); // Fix MS Teams
+user_pref("browser.cache.disk.enable",                                  true); // Fix running out of disk space
